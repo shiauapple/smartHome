37de1da3bf5fa1d1d8cd1a64ae6c0c5a2e94f8ee
shiauapple <shiauapple@outlook.com>
2020-09-28 15:40:27 +0800

    依狀態不同送出 alert, normal 訊息給控制器

--- ArduinoSmartHome/ArduinoSmartHome.ino
+++ ArduinoSmartHome/ArduinoSmartHome.ino
@@ -1,17 +1,20 @@
 void loop()
 {
-if(digitalRead(2)==HIGH) {
-digitalWrite(12, HIGH);
-digitalWrite(11, LOW);
-tone(7, 1000, 500);
-delay(500);
-digitalWrite(12, LOW);
-digitalWrite(11, HIGH);
-tone(7, 800, 500);
-delay(500);
-}
-else {
-digitalWrite(12, LOW);
-digitalWrite(11, LOW);
-}
+  if(digitalRead(2)==HIGH) {
+    Serial.println("alert");
+    digitalWrite(12, HIGH);
+    digitalWrite(11, LOW);
+    tone(7, 1000, 500);
+    delay(500);
+    digitalWrite(12, LOW);
+    digitalWrite(11, HIGH);
+    tone(7, 800, 500);
+    delay(500);
+  }
+  else {
+    Serial.println("normal");
+    digitalWrite(12, LOW);
+    digitalWrite(11, LOW);
+    delay(1000);
+  }
 }
